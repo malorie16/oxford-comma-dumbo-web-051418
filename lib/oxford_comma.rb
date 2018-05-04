2a7dc0ed563d3bdc7d61f2389a29fb48078b14ef
malorie16 <malorie18@gmail.com>
2018-05-04 21:39:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,11 +11,10 @@ def oxford_comma(array)
      array2 = array[3].to_s
      return array1 + " " + array2
   else
-    array1 = array[0..-3]
-    array2 = array[-1]
-    array1.join(", ")
-  array2.insert(0, "and ")
-  return array1 + array2
+    array1 = array[0..-3].join(", ")
+   array2 = array[-1]
+ array2.insert(0, "and ")  
+ return array1 + ", " + array2
 
 end
 end
