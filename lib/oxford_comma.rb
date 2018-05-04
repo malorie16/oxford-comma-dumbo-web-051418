decc6e10ba0287b2b15bc520626bf8fad925184a
malorie16 <malorie18@gmail.com>
2018-05-04 20:29:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length == 1
-    puts array
+    return array
   elsif array.length ==0
-    puts array
+    return array
   elsif array.length == 2
-    puts array
+    return array
   elsif array.length == 3
      array.join( )
 
